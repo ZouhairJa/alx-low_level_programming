e3ab8cc3b93ffa9d6a3cfb9647141a0e80f7810f
ZouhairJa <jaiderzouhair96@gmail.com>
2023-09-08 17:15:54 +0100

    add task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	int i = 122;
-	while (i > 96)
-	{
-		putchar(i);
-		i--;
-	}
-	putchar(10);
-	return (0);
+int i = 122;
+while (i > 96)
+{
+putchar(i);
+i--;
+}
+putchar(10);
+return (0);
 }
