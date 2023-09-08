e9822da8154f52a14b0efb0e3ac49ab7a576a2e7
ZouhairJa <jaiderzouhair96@gmail.com>
2023-09-08 14:11:47 +0100

    add tasks

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,8 +18,7 @@ n = rand() - RAND_MAX / 2;
 if (n == 0)
 printf("%d is zero\n", n);
 else if (n < 0)
-printf("%d is negative\n", n);
-else 
+printf("%d is negative\n", n);else 
 printf("%d is positive\n", n);
 return (0);
 }
