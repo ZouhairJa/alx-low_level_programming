0a38fc67a4b1a809877c56ebe64b2dfadb48d0b2
ZouhairJa <jaiderzouhair96@gmail.com>
2023-09-08 16:57:49 +0100

    add task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,49 +1,21 @@
-<<<<<<< HEAD
-nclude <stdio.h>
-
-/**
- *  * main - print the letters of the alphabet
- *   *
- *    * Description: print the letters of the alphabet in reverse
- *     *
- *      * Return: Always 0 (Success)
- *       */
-
-int main(void)
-{
-		int i = 122;
-
-			while (i > 96)
-					{
-								putchar(i);
-										i--;
-											}
-				putchar(10);
-
-					return (0);
-}
-=======
 #include <stdio.h>
 
 /**
- * main - print the letters of the alphabet
- *
- * Description: print the letters of the alphabet in reverse
- *
- * Return: Always 0 (Success)
- */
+*main - print the letters of the alphabet
+*
+*Description: print the letters of the alphabet in reverse
+*
+*Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	int i = 122;
-
 	while (i > 96)
 	{
 		putchar(i);
 		i--;
 	}
 	putchar(10);
-
 	return (0);
 }
->>>>>>> 329cb316238bea3a2529c67c7bb7afa489d92d9c
