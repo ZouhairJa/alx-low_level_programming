1181a673b34d9326064909598b998594a4c3ddf2
ZouhairJa <jaiderzouhair96@gmail.com>
2023-09-12 10:52:17 +0100

    add 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*main - test that the functiongives the correct output
+*Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
