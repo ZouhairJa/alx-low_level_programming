e5aa7040702b749af2bd249bfb95adab09cf6f26
ZouhairJa <jaiderzouhair96@gmail.com>
2023-09-28 16:52:08 +0100

    add task 5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,9 +9,9 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int omar_needs_this(int n, int s);
+int zouhair_needs_this(int n, int s);
 int is_prime_number(int n);
-int omar_prime_fun(int n, int s);
+int zouhair_prime_fun(int n, int s);
 int is_palindrome(char *s);
-int omar_palindrome(char *s, int i);
+int zouhair_palindrome(char *s, int i);
 #endif
